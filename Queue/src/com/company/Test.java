f3e0282a6073bb5953eb9c4c917d850227ec2007
Administrator <heyukun@iaijian.com>
2018-06-26 14:53:50 +0800

    链表实现队列+test

--- Queue/src/com/company/Test.java
+++ Queue/src/com/company/Test.java
@@ -4,18 +4,23 @@ import java.util.Random;
 
 public class Test {
     public static void main(String[] args) {
-     int opCount = 100000;
-     ArrayQueue<Integer> arrayQueue = new ArrayQueue<>();
-     double time1 = testQueue(arrayQueue,opCount);
-     System.out.println("ArrayQueue ,time: "+time1 +"s");
+        int opCount = 100000;
+        ArrayQueue<Integer> arrayQueue = new ArrayQueue<>();
+        double time1 = testQueue(arrayQueue, opCount);
+        System.out.println("ArrayQueue ,time: " + time1 + "s");
 
-     LoopQueue<Integer> loopQueue = new LoopQueue<>();
-     double time2 = testQueue(loopQueue,opCount);
-     System.out.println("LoopQueue ,time: "+time2 +"s");
+        LoopQueue<Integer> loopQueue = new LoopQueue<>();
+        double time2 = testQueue(loopQueue, opCount);
+        System.out.println("LoopQueue ,time: " + time2 + "s");
+
+
+        LinkedListQueue<Integer> linkedListQueue = new LinkedListQueue<>();
+        double time3 = testQueue(linkedListQueue,opCount);
+        System.out.println("LinkedListQueue ,time: "+time3 +"s");
     }
 
     //测试使用queue运行opCount个enqueue和dequeue操作所需要的时间
-    private static double testQueue(Queue<Integer> queue,int opCount){
+    private static double testQueue(Queue<Integer> queue, int opCount) {
         long startTime = System.nanoTime();
         Random random = new Random();
         for (int i = 0; i < opCount; i++) {
@@ -26,6 +31,6 @@ public class Test {
         }
         //....
         long endTime = System.nanoTime();
-        return (endTime -startTime)/1000000000.0;
+        return (endTime - startTime) / 1000000000.0;
     }
 }
