d5019da5ee9040e02c82d912a26a384baf299fca
Administrator <heyukun@iaijian.com>
2018-06-28 12:24:42 +0800

    BST 前序 中序 后序遍历

--- BST/src/com/company/BST.java
+++ BST/src/com/company/BST.java
@@ -48,6 +48,98 @@ public class BST<E extends Comparable<E>> {
         }
 
         return node;
+    }
 
+    //是否包含元素e
+    public boolean contains(E e) {
+        return contains(root, e);
+    }
+
+    //向以node为根的二分搜索树中查询元素E ,递归
+    private boolean contains(Node node, E e) {
+        if (node == null) {
+            return false;
+        }
+
+        if (e.compareTo(node.e) == 0)
+            return true;
+        else if (e.compareTo(node.e) < 0)
+            return contains(node.left, e);
+        else
+            return contains(node.right, e);
+    }
+
+    //前序遍历
+    public void preOrder() {
+        preOrder(root);
+    }
+
+    private void preOrder(Node node) {
+        //递归终止
+        if (node == null) {
+            return;
+        }
+
+        //递归分解
+        System.out.println(node.e);
+        preOrder(node.left);
+        preOrder(node.right);
+    }
+
+    //中序遍历
+    public void inOrder(){
+        inOrder(root);
+    }
+
+    private void inOrder(Node node){
+        if(node == null){
+            return;
+        }
+
+        inOrder(node.left);
+        System.out.println(node.e);
+        inOrder(node.right);
+    }
+
+
+    //后序遍历
+    public void postOrder(){
+        postOrder(root);
+    }
+
+    private void postOrder(Node node){
+        if(node == null){
+            return;
+        }
+
+        postOrder(node.left);
+        postOrder(node.right);
+        System.out.println(node.e);
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder res = new StringBuilder();
+        generateBSTString(root, 0, res);
+        return res.toString();
+    }
+
+    private void generateBSTString(Node node, int depth, StringBuilder res) {
+        if (node == null) {
+            res.append(generateDepthString(depth) + "null\n");
+            return;
+        }
+
+        res.append(generateDepthString(depth)).append(node.e).append("\n");
+        generateBSTString(node.left, depth + 1, res);
+        generateBSTString(node.right, depth + 1, res);
+    }
+
+    private String generateDepthString(int depth) {
+        StringBuilder res = new StringBuilder();
+        for (int i = 0; i < depth; i++) {
+            res.append("--");
+        }
+        return res.toString();
     }
 }
--- BST/src/com/company/Main.java
+++ BST/src/com/company/Main.java
@@ -3,6 +3,25 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        // write your code here
+        BST<Integer> bst = new BST<>();
+        int[] nums = {5, 3, 6, 8, 4, 2};
+        for (int num : nums) {
+            bst.add(num);
+            /////////////////////
+            //        5        //
+            //      /   \      //
+            //     3     6     //
+            //    / \     \    //
+            //   2  4      8   //
+            ////////////////////
+        }
+        System.out.println("----------前序遍历-----------");
+        bst.preOrder();
+        System.out.println("----------中序遍历-----------");
+        bst.inOrder();
+        System.out.println("----------后序遍历----------");
+        bst.postOrder();
+//        System.out.println(bst);
     }
 }
