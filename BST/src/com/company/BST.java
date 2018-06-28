028792e4e7b8fb69df911362c46d06bcec6b01fb
Administrator <heyukun@iaijian.com>
2018-06-28 11:18:15 +0800

    BST add()简化代码

--- BST/src/com/company/BST.java
+++ BST/src/com/company/BST.java
@@ -29,34 +29,25 @@ public class BST<E extends Comparable<E>> {
         return size == 0;
     }
 
-    //
+    //添加元素e
     public void add(E e) {
-        if (root == null) {
-            root = new Node(e);
-            size++;
-        } else {
-            add(root, e);
-        }
+        root = add(root, e);
     }
 
     //向以node为根的二分搜索树中插入元素E ,递归
-    private void add(Node node, E e) {
-        if (e.equals(node.e)) {
-            return;
-        } else if (e.compareTo(node.e) < 0 && node.left == null) {
-            node.left = new Node(e);
+    //返回插入新节点二分搜索树的根
+    private Node add(Node node, E e) {
+        if (node == null) {
             size++;
-            return;
-        } else if (e.compareTo(node.e) > 0 && node.right == null) {
-            node.right = new Node(e);
-            size++;
-            return;
+            return new Node(e);
         }
         if (e.compareTo(node.e) < 0) {
-            add(node.left, e);
-        } else {
-            add(node.right, e);
+            node.left = add(node.left, e);
+        } else if (e.compareTo(node.e) > 0) {
+            node.right = add(node.right, e);
         }
+
+        return node;
 
     }
 }
