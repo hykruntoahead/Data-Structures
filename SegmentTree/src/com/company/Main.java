b818db0b979dd32a46c3e8577395011c84efb53a
Administrator <heyukun@iaijian.com>
2018-07-16 12:11:38 +0800

    线段树的创建 与查询

--- SegmentTree/src/com/company/Main.java
+++ SegmentTree/src/com/company/Main.java
@@ -3,6 +3,18 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        // write your code here
+        Integer[] nums = {-2, 0, 3, -5, 2, -1};
+        SegmentTree<Integer> segmentTree = new SegmentTree<>(
+                nums, new Merger<Integer>() {
+            @Override
+            public Integer merge(Integer a, Integer b) {
+                return a + b;
+            }
+        });
+//        System.out.println("segmentTree = " + segmentTree);
+
+        System.out.println(segmentTree.query(0,2));
+        System.out.println(segmentTree.query(2,5));
     }
 }
