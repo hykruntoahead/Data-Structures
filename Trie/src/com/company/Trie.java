06c0088ce141d857a49301eb0781e26741efdf1b
Administrator <heyukun@iaijian.com>
2018-07-18 14:51:07 +0800

    leedcode_677 mapsum

--- Trie/src/com/company/Trie.java
+++ Trie/src/com/company/Trie.java
@@ -71,4 +71,6 @@ public class Trie {
         return true;
     }
 
+    
+
 }
--- Trie/src/com/company/leedcode_677/MapSum.java
+++ Trie/src/com/company/leedcode_677/MapSum.java
@@ -1,17 +1,67 @@
 package com.company.leedcode_677;
 
+
+import java.util.TreeMap;
+
 class MapSum {
 
-    /** Initialize your data structure here. */
+    private class Node {
+        public int value;
+        public TreeMap<Character, Node> next;
+
+        public Node(int value) {
+            this.value = value;
+            next = new TreeMap<>();
+        }
+
+        public Node() {
+            this(0);
+        }
+    }
+
+    private Node root;
+
+    /**
+     * Initialize your data structure here.
+     */
     public MapSum() {
-        
+        root = new Node();
     }
-    
-    public void insert(String key, int val) {
-        
+
+    public void insert(String word, int val) {
+        Node cur = root;
+        for (int i = 0; i < word.length(); i++) {
+            char c = word.charAt(i);
+            if (cur.next.get(c) == null) {
+                cur.next.put(c, new Node());
+            }
+            cur = cur.next.get(c);
+        }
+        cur.value = val;
     }
-    
+
     public int sum(String prefix) {
-        return 0;
+        Node cur = root;
+        for (int i = 0; i < prefix.length(); i++) {
+            char c = prefix.charAt(i);
+            if (cur.next.get(c) == null) {
+                return 0;
+            }
+            cur = cur.next.get(c);
+        }
+
+        return sum(cur);
     }
+
+    private int sum(Node node) {
+        if (node.next.size() == 0)
+            return node.value;
+
+        int res = node.value;
+        for (char c : node.next.keySet()) {
+            res += sum(node.next.get(c));
+        }
+        return res;
+    }
+
 }
